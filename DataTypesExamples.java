6d227585c7ed4d39630df1e6884058d3d2674235
Ghulam Mustafa <148903047+gmbalouch@users.noreply.github.com>
2024-02-24 17:08:05 +0500

    Add files via upload
    Understanding Data Types: Dive into the world of data types in Java, including integral types (byte, short, int, long), floating-point types (float, double), char, and boolean. Discover how each type handles different kinds of data and when to use them.
    
    Working with Variables: Explore the concept of variables and how they act as containers for storing data. Learn the rules for naming variables, initializing them, and discover the scope of variables in Java programs.
    
    Common Coding Patterns: Witness practical examples and coding patterns that demonstrate the use of data types and variables in real-world scenarios. Gain hands-on experience to solidify your understanding.

---
+++ DataTypesExamples.java
@@ -0,0 +1,20 @@
+
+public class DataTypesExamples {
+
+	public static void main(String[] args) {
+		byte a = 127;       
+		int num=4326;
+		
+		double d=15.56;
+		float f=12.43f;
+		
+		char c='a';
+		
+		System.out.println("The value of a variable "+a);
+		System.out.println("The value of num variable "+num);
+		System.out.println("The value of d variable "+d);
+		System.out.println("The value of f variable "+f);
+		System.out.println("The value of c variable "+c);
+	}
+
+}
