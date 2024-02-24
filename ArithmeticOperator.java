0262dc1d5cbafcc9b7ad4aabaa438b86b25f47e9
Ghulam Mustafa <148903047+gmbalouch@users.noreply.github.com>
2024-02-24 16:14:45 +0500

    Add files via upload
    These both files are from lecture: 1
    
    Also well explained on Youtube : https://www.youtube.com/playlist?list=PLYR_JD4qS7B1ofQk8zGKBLD7dX2iKRzwc

---
+++ ArithmeticOperator.java
@@ -0,0 +1,13 @@
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		
+		System.out.println(6+6);
+		System.out.println(6-6);
+		System.out.println(6*6);
+		System.out.println(6/6);
+		System.out.println(6%6);
+	}
+
+}
