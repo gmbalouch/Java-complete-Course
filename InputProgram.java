31b93fa06b95ce644301dc717dc832c445c1ae94
Ghulam Mustafa <148903047+gmbalouch@users.noreply.github.com>
2024-02-27 19:49:06 +0500

    Add files via upload
    This is first program for taking input from user using command line

---
+++ InputProgram.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class InputProgram {
+
+	public static void main(String[] args) {
+		
+		Scanner object=new Scanner(System.in);
+		
+		System.out.println("Enter any number");
+		int a=object.nextInt();
+		System.out.println(a*a);
+		
+
+	}
+
+}
